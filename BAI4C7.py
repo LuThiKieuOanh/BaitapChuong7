cc436e5df27ad31b282e12eb84b29c3924b9469b
LuThiKieuOanh <lukieuoanh163@gmail.com>
2024-09-30 12:14:03 +0700

    Add files via upload

---
+++ BAI4C7.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 30 12:07:57 2024
+
+@author: KieuOanh
+"""
+
+ds = [('Tiền Giang', 63), ('Long An', 62), ('Vĩnh Long', 64), ('Bình Dương', 60)]
+print(sorted(ds))
